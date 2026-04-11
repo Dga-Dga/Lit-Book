825a2bf935c4f1ce2053bf7bb4dcb73692858cce
EgorKryuchkoff <117818024+Dga-Dga@users.noreply.github.com>
2026-04-11 13:09:14 +0000

    day two (1)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: '0.0.0.0',           // слушать все сетевые интерфейсы
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: process.env.CODESPACE_NAME 
+            ? `${process.env.CODESPACE_NAME}-5173.app.github.dev`
+            : 'localhost',
+            protocol: 'wss',
+            clientPort: 443,
+        },
+    },
+});
 // import { defineConfig } from 'vite';
 // import laravel from 'laravel-vite-plugin';
 // import tailwindcss from '@tailwindcss/vite';
@@ -16,27 +41,3 @@
 //         },
 //     },
 // });
-
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: '0.0.0.0',           // слушать все сетевые интерфейсы
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: process.env.CODESPACE_NAME 
-                ? `${process.env.CODESPACE_NAME}-5173.app.github.dev`
-                : 'localhost',
-            protocol: 'wss',
-            clientPort: 443,
-        },
-    },
-});
